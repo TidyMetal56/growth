5523dba5fd9fafb99ffea1b6d0786276ade30971
TidyMetal56 <carlosvera_99@hotmail.com>
2023-05-06 13:05:14 -0500

    e

---
+++ app/Http/Controllers/TaxeTController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Database\migrations\federal;
+
+class TaxeTController extends Controller
+{
+    protected $table = '_federal_taxes';
+}
--- resources/js/components/app.jsx
+++ resources/js/components/app.jsx
@@ -1,19 +1,31 @@
-import { Component } from 'react';
-import '../bootstrap';
-import ReactDOM  from 'react-dom';
-import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Component } from "react";
+import "../bootstrap";
+import ReactDOM from "react-dom";
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
 
-class App extends Component{
-    render() {
-        return (
-        <Home/>
-        );
-    }
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
-export default App;
-
-if(document.getElementById("app")){
-    ReactDOM.render(<App />,document.getElementById("app"))
+if (document.getElementById("app")) {
+  ReactDOM.render(<App />, document.getElementById("app"));
 }
-
